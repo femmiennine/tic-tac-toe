356e7521d4ed3dac50d6821541cb99afc0bf1f1b
Frances Fogarty <frances.fogarty@ringtailinteractive.com>
2023-08-02 09:44:45 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            SetField();
+            Console.ReadKey();
+        }
+
+        public static void SetField()
+        {
+            Console.WriteLine("     |     |     ");
+            // TODO replace numbers with variables
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            // TODO replace numbers with variables
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            // TODO replace numbers with variables
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("     |     |     ");
+        }
+    }
+}
